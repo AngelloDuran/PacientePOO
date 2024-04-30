da4fd30d9aff6fe13a30824a9bd2a5e2cc5b4e0c
AngelloDuran <duea020109hdfrsna5@gmail.com>
2024-04-29 23:59:17 -0600

    PacientePrivate

--- MainPaciente.java
+++ MainPaciente.java
@@ -4,22 +4,42 @@ public class MainPaciente {
 
     public static void main(String[] args) {
         Scanner p1 = new Scanner(System.in);
-        System.out.println("ingresa tu nombre:");
-        String name = p1.next();
-        System.out.println("ingresa sus apellido:");
-        String lastname = p1.next();
-        System.out.println("ingresa su edad:");
+        System.out.println("ingresa tu nombre");
+        String nombre = p1.next();
+        System.out.println("ingresa tu apellido");
+        String apellido = p1.next();
+        System.out.println("ingresa tu edad ");
         int edad = p1.nextInt();
-        System.out.println("ingresa su altura:");
+        System.out.println("ingresa tu altura");
         double altura = p1.nextDouble();
         Paciente p2 = new Paciente();
-        p2.nombre = name;
-        p2.apellido = lastname;
-        p2.edad = edad;
-        p2.altura = altura;
-        p2.mostrarNombre();
-        p2.mostrarApellido();
-        p2.mostrarAltura();
+        p2.setNombre(nombre);
+        p2.setApellido(apellido);
+        p2.setEdad(edad);
+        p2.setAltura(altura);
+        p2.getNombre();
+        p2.getApellido();
+        p2.getAltura();
+        System.out.println(p2.toString());
+        Paciente p3 = new Paciente();
+        System.out.println("ingresa tu nombre");
+        p1.nextLine();
+        String nombre2 = p1.next();
+        System.out.println("ingresa tu apellido");
+        String apellido2 = p1.next();
+        System.out.println("ingresa tu edad ");
+        int edad2 = p1.nextInt();
+        System.out.println("ingresa tu altura");
+        double altura2 = p1.nextDouble();
+        p3.setNombre(nombre2);
+        p3.setApellido(apellido2);
+        p3.setEdad(edad2);
+        p3.setAltura(altura2);
+        p3.getNombre();
+        p3.getApellido();
+        p3.getAltura();
+        System.out.println(p3.toString());
+
     }
 
 }
--- Paciente.java
+++ Paciente.java
@@ -1,18 +1,66 @@
 public class Paciente {
-    String nombre;
-    String apellido;
-    int edad;
-    double altura;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private double altura;
 
     public void mostrarNombre() {
-        System.out.println("el nombre del paciente es:" + this.nombre);
+        System.out.println("mi nombre es:" + this.nombre);
     }
 
     public void mostrarApellido() {
-        System.out.println("apellidos del paciente es:" + this.apellido);
+        System.out.println("mi apellido es:" + this.apellido);
     }
 
     public void mostrarAltura() {
-        System.out.println("su altura es:" + this.altura);
+        System.out.println("mi altura es:" + this.altura);
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad < 0) {
+            System.out.println(" edad es invilida");
+
+        } else {
+            this.edad = edad;
+        }
+
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura < 0) {
+            System.out.println("altura invalida");
+        } else {
+            this.altura = altura;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", altura=" + altura + "]";
+    }
+
 }
